a7c45a931adb3b444956c92388c194711a285574
ryakubovsky <data.yakubovsky.rom@gmail.com>
2020-09-15 21:14:36 +0300

    test bq

--- reports/timestamp_to_bq.py
+++ reports/timestamp_to_bq.py
@@ -5,7 +5,7 @@ from connectors._BigQuery import BigQuery
 from datetime import datetime
 
 
-path_to_bq = "../json_files/rta-moscow20.json"
+path_to_bq = "/home/rta_analytics/MSAgency/json_files/rta-moscow20.json"
 bq = BigQuery(path_to_bq)
 time_var = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
 bq.insert_json("TimeTest", "TimeTest", [{"timestamp_var": time_var}])
